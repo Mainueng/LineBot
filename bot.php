b42e483cf0be6774d368811933d085a9ecc9f107
Nueng <5303phanat@gmail.com>
2017-07-20 15:40:57 +0700

    ssss

--- bot.php
+++ bot.php
@@ -13,20 +13,16 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
-
-			if ('text' = "123") {
-			
 			// Get replyToken
-			$replyToken = $event['replyToken'];
 
+			if ('text' == "123"  ){
+			$replyToken = $event['replyToken'];
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
 				'text' => "HOOOOOOOOO"
 			];
-
 		}
-
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
