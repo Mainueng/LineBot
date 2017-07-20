9364b068c91a868f2dc038d74c3561fe613f7c33
Nueng <5303phanat@gmail.com>
2017-07-20 19:45:43 +0700

    commit 2

--- bot.php
+++ bot.php
@@ -36,6 +36,26 @@ if (!is_null($events['events'])) {
 					'text' => "เปิดไฟห้องนอนเรียบร้อย"
 				];
 			}
+			else if ($text == "ปิดไฟห้องนั่ง้เล่น"){
+				$replyToken = $event['replyToken'];
+				$ch = curl_init("https://api.anto.io/channel/set/a67sMedxyPSvQ8Lo1ZEc2DrItGgYpiATFnqkzbJK/NodeMCU/Living_Room/0");
+				curl_exec($ch);
+
+				$messages = [
+					'type' => 'text',
+					'text' => "ปิดไฟห้องนั่งเล่นเรียบร้อย"
+				];
+			}
+			else if ($text == "เปิดไฟห้องนั่ง้เล่น"){
+				$replyToken = $event['replyToken'];
+				$ch = curl_init("https://api.anto.io/channel/set/a67sMedxyPSvQ8Lo1ZEc2DrItGgYpiATFnqkzbJK/NodeMCU/Living_Room/1");
+				curl_exec($ch);
+
+				$messages = [
+					'type' => 'text',
+					'text' => "เปิดไฟห้องนั่งเล่นเรียบร้อย"
+				];
+			}
 			else{
 				$replyToken = $event['replyToken'];
 				$messages = [
