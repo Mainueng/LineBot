94ab2330a30f41c4e7627f558fd3b6bcfede4e8c
Nueng <5303phanat@gmail.com>
2017-07-26 16:10:00 +0700

    Sticker

--- bot.php
+++ bot.php
@@ -21,11 +21,11 @@ if (!is_null($events['events'])) {
 				$ch = curl_init("https://api.anto.io/channel/set/a67sMedxyPSvQ8Lo1ZEc2DrItGgYpiATFnqkzbJK/NodeMCU/Bed_Room/0");
 				curl_exec($ch);
 
-				$messages2 = [
+				$messages = [
 					'type' => 'text',
 					'text' => "ปิดไฟห้องนอนเรียบร้อย"
 				];
-				$messages = [
+				$messages2 = [
 					'type' => 'sticker',
 					'packageId' => '1',
     				'stickerId' => '2'
@@ -75,8 +75,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
-				'messages' => [$messages2],
+				'messages' => [$messages],[$messages2]
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
