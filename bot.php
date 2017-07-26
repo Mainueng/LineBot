8767d0e3624e9ecd80dc789a02732add3236a8a7
Nueng <5303phanat@gmail.com>
2017-07-26 15:15:33 +0700

    Sticker

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@ if (!is_null($events['events'])) {
 				$messages = [
 					'type' => 'sticker',
 					'packageId': "1",
-    				'stickerId': "1"
+    				'stickerId': "1"//
     			];
 			}
 			else if ($text == "เปิดไฟห้องนอน"){
