42251642e1eb5ab02c173965f30eaab624d2321b
Nueng <5303phanat@gmail.com>
2017-07-26 15:48:23 +0700

    Sticker

--- bot.php
+++ bot.php
@@ -65,7 +65,7 @@ if (!is_null($events['events'])) {
 				$messages = [
 					'type' => 'sticker',
 					'packageId'=> '1',
-    				'stickerId'=> '40'//
+    				'stickerId'=> '2'//
     			];
 			}
 
