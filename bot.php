02df43764b48f1be95d411f94d64ffe24134d46c
Nueng <5303phanat@gmail.com>
2017-07-28 18:10:55 +0700

    Cal

--- bot.php
+++ bot.php
@@ -46,7 +46,7 @@ if (!is_null($events['events'])) {
 					$messages3 = [
 						'type' => 'sticker',
 						'packageId' => '1',
-    					'stickerId' => '3'
+    					'stickerId' => '4'
 						];
 				}
 				else if ($cal > 999999999999999999){
