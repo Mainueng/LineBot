090e5047b514480f3ec9ba178194d3b2e70db2d1
Nueng <5303phanat@gmail.com>
2017-07-28 14:36:50 +0700

    ca

--- bot.php
+++ bot.php
@@ -35,7 +35,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages,$messages2],
+				'messages' => [$messages],
 			
 			];
 			$post = json_encode($data);
