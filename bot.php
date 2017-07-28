f8188ebaefffdd820af5749117c5f35961fdf05f
Nueng <5303phanat@gmail.com>
2017-07-28 14:45:25 +0700

    ca

--- bot.php
+++ bot.php
@@ -17,13 +17,22 @@ if (!is_null($events['events'])) {
 
 
 			if (strpos($text, '=') !== false) {
-
-			$symbol = explode(" ", $text);
-				
+				$symbol = explode(" ", $text);
+					if($symbol[1] == "+"){
+						$cal = intval($symbol[0]) + intval($symbol[2]);
+					}
+					else if($symbol[1] == "-"){
+						$cal = intval($symbol[0]) - intval($symbol[2]);	
+					}
+					else if($symbol[1] == "*"){
+						$cal = intval($symbol[0]) * intval($symbol[2]);	
+					}
+					else if($symbol[1] == "/"){
+						$cal = intval($symbol[0]) / intval($symbol[2]);	
 					$replyToken = $event['replyToken'];
 					$messages = [
 						'type' => 'text',
-						'text' => $symbol[0]
+						'text' => $cal
 				]; 
 				}
 
