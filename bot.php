3515a5a23f504d60bb7e19ea0c54c3658e340092
Nueng <5303phanat@gmail.com>
2017-07-28 17:33:28 +0700

    Cal

--- bot.php
+++ bot.php
@@ -48,6 +48,21 @@ if (!is_null($events['events'])) {
 						];
 			}
 
+			else{
+				$messages = [
+						'type' => 'text',
+						'text' => $text
+				];
+				$messages2 = [
+						'type' => 'text',
+						'text' => null
+				]; 
+				$messages3 = [
+						'type' => 'text',
+						'text' => null
+				];  
+			}
+
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
