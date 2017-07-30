42835d9605d2a42e1b199b2a8677be400f62d151
Nueng <5303phanat@gmail.com>
2017-07-30 14:11:14 +0700

    DB

--- verify.php
+++ verify.php
@@ -8,6 +8,19 @@ $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
+$url2 = parse_url(getenv("mysql://b92d9507302d3f:83435ac5@us-cdbr-iron-east-03.cleardb.net/heroku_f10f824e36ff3bf?reconnect=true"));
+$server = $url2["us-cdbr-iron-east-03.cleardb.net"];
+$username = $url2["b92d9507302d3f"];
+$password = $url2["83435ac5"];
+$db = substr($url2["heroku_f10f824e36ff3bf"], 1);
+
+$conn = new mysqli($server, $username, $password, $db)
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully"
+$conn->close();
+
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
@@ -18,3 +31,5 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
+
+
