a516bf4366d2c4323f854e708aece176fc35c7ec
Nueng <5303phanat@gmail.com>
2017-07-31 16:52:04 +0700

    DB

--- verify.php
+++ verify.php
@@ -15,9 +15,10 @@ $conn = new mysqli($server, $username, $password, $db);
 		echo "Connected successfully (".$conn->host_info.")";
 	}
 
-	$sql = "SELECT * FROM heroku_f10f824e36ff3bf.learn;";
-
-	echo "<br> Answer:".["Answer"]."<br>";
+	$sql = "SELECT * FROM heroku_f10f824e36ff3bf.learn";
+		while($row = mysqli_fetch_array($sql)){
+			echo "<br> Answer:".$row[('Answer')]."<br>";
+		}
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
@@ -33,3 +34,4 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
+
