61d627a7aad72ae8cad8efd4b97d9768a16a6fed
Nueng <5303phanat@gmail.com>
2017-08-01 17:33:50 +0700

    MongoDB

--- bot.php
+++ bot.php
@@ -128,7 +128,7 @@ if (!is_null($events['events'])) {
 				];
 					$messages3 = [
 						'type' => 'text',
-						'text' => "2"
+						'text' => ""
 				];
 			}
 		}	
@@ -147,7 +147,7 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, json_encode($headers));
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			curl_setopt($ch, CURLOPT_PROXY, $proxy);
 			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxnueng);
