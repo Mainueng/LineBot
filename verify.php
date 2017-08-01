2ed309395fb5c1bf9a271db47f9ed95a0d1d3fd2
Nueng <5303phanat@gmail.com>
2017-08-01 20:52:15 +0700

    mLab

--- verify.php
+++ verify.php
@@ -11,10 +11,10 @@ $headers = array('Authorization: Bearer ' . $access_token);
 $text = "สอนไม้หนึ่ง[สวัสดีจ้า|สวัสดีครับ]";
 $extra = str_replace("สอนไม้หนึ่ง","", $text);
 				$words = explode("|", $extra);
-    			//$_question = str_replace("[","",$words[0]);
-    			//$_answer = str_replace("]","",$words[1]);
-echo $words[0];
-echo $words[1];
+    			$_question = str_replace("[","",$words[0]);
+    			$_answer = str_replace("]","",$words[1]);
+echo $_question;
+echo $_answer;
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
