abf279fc64480b15862540403071683031042afb
Nueng <5303phanat@gmail.com>
2017-08-01 17:27:12 +0700

    MongoDB

--- bot.php
+++ bot.php
@@ -124,11 +124,11 @@ if (!is_null($events['events'])) {
 				]; 
 					$messages2 = [
 						'type' => 'text',
-						'text' => "4"
+						'text' => " "
 				];
 					$messages3 = [
 						'type' => 'text',
-						'text' => "5"
+						'text' => " "
 				];
 			}
 		}	
